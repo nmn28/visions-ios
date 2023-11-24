42466e4ee51e17dbee9821393bc933fffb5579a1
Nicholas Nelson <nicholasmnelson10@gmail.com>
2023-11-23 19:52:51 -0800

    Initial Commit

---
+++ market/marketApp.swift
@@ -0,0 +1,17 @@
+//
+//  marketApp.swift
+//  market
+//
+//  Created by Nicholas Nelson on 11/23/23.
+//
+
+import SwiftUI
+
+@main
+struct marketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
